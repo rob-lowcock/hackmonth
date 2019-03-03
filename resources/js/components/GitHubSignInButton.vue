e87e4856fe6d0ed702df2f78b24be01d11f2e985
Rob <rob@Robs-Air.lan>
2019-03-03 21:38:48 +0000

    Initial commit

---
+++ resources/js/components/GitHubSignInButton.vue
@@ -0,0 +1,31 @@
+<style scoped>
+    .github-sign-in {
+        background: #fff;
+        border: 1px solid #ccc;
+        background: linear-gradient(0deg, #eee 0%, #fff 100%);
+
+        border-radius: 4px;
+        padding: 6px 10px;
+        font-size: 1.1em;
+    }
+
+    i {
+        padding-right: 5px;
+        font-size: 1.4em;
+        vertical-align: -2px;
+    }
+</style>
+
+<template>
+    <button class="github-sign-in">
+        <i class="fab fa-github"></i> Sign In with GitHub
+    </button>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Button mounted.')
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>HackMonth 2019</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div id="app">
+            <div class="content billboard">
+                <div class="container">
+                    <div class="call-to-action float-right card">
+                        <div class="card-body">
+                            <h4>Get Started</h4>
+                            <github-sign-in-button></github-sign-in-button>
+                        </div>
+                    </div>
+                    <h1>HackMonth 2019</h1>
+                    <h2>Challenge yourself to code something in 30 days</h2>
+                </div>
+            </div>
+            <div class="content">
+                <div class="container">
+                    <div class="how-it-works">
+                        <h1>How it works</h1>
+                        <ul>
+                            <li><i class="fas fa-rocket"></i> Build a launch a project in just one month</li>
+                            <li><i class="fab fa-github"></i> Link your GitHub account to track progress</li>
+                            <li><i class="fas fa-users"></i> Get support and encouragement from other people attempting the same thing</li>
+                            <li><i class="fas fa-flag-checkered"></i> When you tag your v1 release, you're done!</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
