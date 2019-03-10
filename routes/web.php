ecb300b02fa5a0cf22f79b283cf37a40dad705ad
Rob <rob.lowcock@gmail.com>
2019-03-10 20:32:41 +0000

    Add GitHub integration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
